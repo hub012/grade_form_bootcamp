c9dbc53058f38d1b2f9485c1bdca686cbcf33a7b
Abel Quinones <hub0121@gmail.com>
2022-07-10 12:01:53 -0400

    Actualizado

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
+// 1- Crear clase con los siguentes atributos: Nombre Apellido Matricula Nota
+// 2- Crear funcion para promediar notas.
+// 3- Agregar estudiantes de ejemplos.
+//-----------------------------------------------------------------------------
+// 4- Agregar Boton de editar.
+// 5- Agregar boton de eliminar.
+// 6- Agregar funcion de grabar en local estorage.
+
+
+
 let notas=[];
 
-    
-// 1- Crear clase con los siguentes atributos: Nombre Apellido Matricula Nota
-// 2- Crear funcion para promediar notas
-// 3- Agregar estudiantes de ejemplos
 
+class Estudiante {
+    constructor(nombre, apellido,matricula, nota){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.matricula=matricula;
+        this.nota=nota;
+    }
+}
 
+//crear tablas
 
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+// Adding the entire table to the body tag
+document.getElementById('body').appendChild(table);
+
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "Nombre";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "Apellido";
+let heading_3 = document.createElement('th');
+heading_3.innerHTML = "Matricula";
+let heading_4 = document.createElement('th');
+heading_4.innerHTML = "Nota";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+row_1.appendChild(heading_4);
+thead.appendChild(row_1);
